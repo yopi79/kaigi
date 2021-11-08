87da9f19725c2254e3170096030ac6de2a1ef1cf
yopi79 <77525734+yopi79@users.noreply.github.com>
2021-11-08 11:37:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const Peer = window.Peer;
   const leaveTrigger = document.getElementById('js-leave-trigger');
   const micTrigger = document.getElementById('js-mic-trigger');
   const videoTrigger = document.getElementById('js-video-trigger');
+  const shareTrigger = document.getElementById('js-shere-trigger');
   const remoteVideos = document.getElementById('js-remote-streams');
   const roomId = document.getElementById('js-room-id');
   const roomMode = document.getElementById('js-room-mode');
   const localText = document.getElementById('js-local-text');
+  const localName = document.getElementById('js-local-name');
   const sendTrigger = document.getElementById('js-send-trigger');
   const messages = document.getElementById('js-messages');
   const meta = document.getElementById('js-meta');
@@ -48,6 +50,10 @@ const Peer = window.Peer;
   localVideo.srcObject = localStream;
   localVideo.playsInline = true;
   await localVideo.play().catch(console.error);
+  //ビデオ・マイクをデフォルトでoffにする
+  localStream.getVideoTracks().forEach((video) => (video.enabled = false));
+  localStream.getAudioTracks().forEach((track) => (track.enabled = false));
+
 
   // Peerのインスタンス作成
   const peer = (window.peer = new Peer({
@@ -86,8 +92,8 @@ const Peer = window.Peer;
     });
 
     //重要：誰かがテキストメッセージを送った時、messagesを更新
-    room.on('data', ({ data, src }) => {
-      messages.textContent += `${src}: ${data}\n`;
+    room.on('data', ({ data }) => {
+      messages.textContent += `${data}\n`;
     });
 
      // 誰かが退出した場合、div（remoteVideos）内にある、任意のdata-peer-idがついたvideoタグの内容を空にして削除する
@@ -123,40 +129,87 @@ const Peer = window.Peer;
     leaveTrigger.addEventListener('click', () => room.close(), { once: true });
     micTrigger.addEventListener('click', muted);
     videoTrigger.addEventListener('click', videoff);
+    //shareTrigger.addEventListener('click', shareDisplay);
 
     //テキストメッセージを送る処理
     function onClickSend() {
-      room.send(localText.value);
-      messages.textContent += `${peer.id}: ${localText.value}\n`;
+      room.send(localName.value+": "+localText.value);
+      messages.textContent += `${localName.value}: ${localText.value}\n`;
       localText.value = '';
     }
     function muted() {
       // 音声のみミュート
       if(mic){
-        localStream.getAudioTracks().forEach((track) => (track.enabled = false));
-        document.getElementById("js-mic-trigger").textContent="mic-off"
+        localStream.getAudioTracks().forEach((track) => (track.enabled = true));
+        document.getElementById("js-mic-trigger").innerHTML='<img src="img/micon.png">';
         mic=false;
       }
       else{
-        localStream.getAudioTracks().forEach((track) => (track.enabled = true));
-        document.getElementById("js-mic-trigger").textContent="mic-on "
+        localStream.getAudioTracks().forEach((track) => (track.enabled = false));
+        document.getElementById("js-mic-trigger").innerHTML='<img src="img/micoff.png">';
         mic=true;
       }
     }
     function videoff() {
       //ビデオのみミュート
       if(video){
-        localStream.getVideoTracks().forEach((video) => (video.enabled = false));
-        document.getElementById("js-video-trigger").textContent="video-off"
+        localStream.getVideoTracks().forEach((video) => (video.enabled = true));
+        document.getElementById("js-video-trigger").innerHTML='<img src="img/videon.png">';
         video=false;
       }
+      //カメラオン
       else{
-        localStream.getVideoTracks().forEach((video) => (video.enabled = true));
-        document.getElementById("js-video-trigger").textContent="video-on "
+        localStream.getVideoTracks().forEach((video) => (video.enabled = false));
+        document.getElementById("js-video-trigger").innerHTML='<img src="img/videoff.png">';
         video=true;
       }
     }
-  });
 
+      //以下画面共有(できない)
+    function handleSuccess(stream) {
+      startButton.disabled = true;
+      const video = document.querySelector('video');
+      video.srcObject = stream;
+    
+      // demonstrates how to detect that the user has stopped
+      // sharing the screen via the browser UI.
+      stream.getVideoTracks()[0].addEventListener('ended', () => {
+        errorMsg('The user has ended sharing the screen');
+        startButton.disabled = false;
+      });
+    }
+    
+    function handleError(error) {
+      errorMsg(`getDisplayMedia error: ${error.name}`, error);
+    }
+    
+    function errorMsg(msg, error) {
+      const errorElement = document.querySelector('#errorMsg');
+      errorElement.innerHTML += `<p>${msg}</p>`;
+      if (typeof error !== 'undefined') {
+        console.error(error);
+      }
+    }
+    
+    const startButton = document.getElementById('js-share-trigger');
+    startButton.addEventListener('click', () => {
+      navigator.mediaDevices.getDisplayMedia({video: true})
+          .then(handleSuccess, handleError);
+    });
+    
+    if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+      startButton.disabled = false;
+    } else {
+      errorMsg('getDisplayMedia is not supported');
+    }
+  })
   peer.on('error', console.error);
 })();
+
+//画面共有
+// Polyfill in Firefox.
+// See https://blog.mozilla.org/webrtc/getdisplaymedia-now-available-in-adapter-js/
+// if (adapter.browserDetails.browser == 'firefox') {
+//   adapter.browserShim.shimGetDisplayMedia(window, 'screen');
+// }
+
